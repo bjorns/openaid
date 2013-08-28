3994c74b1fffd46488220afde21c68e14575dcc7
Björn Skoglund <bjorn.skoglund@valtech.se>
2013-08-28 10:51:49 +0200

    Added dropdown with all countries.

--- openaid.js
+++ openaid.js
@@ -22,52 +22,109 @@
 // }
 // var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Line(lineChartData);
 
+var graph = null;
 
 var contributions = function(response) {
 	return response[0].extra_data.contributions_by_year
 };
 
-var sum = function(data) {
-	var ret = 0
-	$(data).each(function(i,v) {ret+=parseInt(v)})
-	return ret
+var sumContributions = function(year, ids, contributions) {
+	var sum = 0;
+	$(ids).each(function(i,id){
+		var c = contributions[id];
+		if (c!=undefined) {
+			var o = c.outcome[year];
+			if (o === '') {
+				o = '0'
+			}
+			if (year == "2004" && c.name == "SEN Transportsystem") {
+				
+				console.log("=== " + c.name + "\t\t\t\t\t\t" + o)
+				
+			}
+			sum += parseInt(o);
+		}
+	});
+	return sum;
 }
 
-var render = function(labels, values) {
+var totalPerYear = function(info, contributions) {
+	var ret = {};
+	$.each(info.extra_data.contributions_by_year, function(k,v){
+		ret[k] = sumContributions(k, v, contributions)
+	});
+	return ret;
+}
+
+var values = function(object) {
+	var ret = [];
+	$.each(object, function(k,v){
+		ret.push(v);
+	});
+	return ret;
+}
+
+var dataset = function(values, r, g, b) {
+	return {
+ 		fillColor : "rgba(" + r + "," + g + "," + b + ",0.5)",
+ 		strokeColor : "rgba(" + r + "," + g + "," + b + ",1)",
+	 	pointColor : "rgba(" + r + "," + g + "," + b + ",1)",
+ 		pointStrokeColor : "#fff",
+ 		data : values
+	 };
+}
+
+var render = function(labels, datasets) {
+	if (graph == null) {
+ 		graph = new Chart(document.getElementById("canvas").getContext("2d"));
+ 	}
 	var lineChartData = {
 		labels: labels,
-		datasets : [
- 			{
- 				fillColor : "rgba(220,220,220,0.5)",
- 				strokeColor : "rgba(220,220,220,1)",
-	 			pointColor : "rgba(220,220,220,1)",
- 				pointStrokeColor : "#fff",
- 				data : values
-	 		}
- 		]
- 	};
- 	var graph = new Chart(document.getElementById("canvas").getContext("2d")).Line(lineChartData);
+		datasets : datasets
+	};
+
+ 	graph.Line(lineChartData);
+}
+
+var fetch = function(url) {
+	var info = {};
+	var call = $.ajax({
+		url: url,
+		dataType: "json",
+		async: false
+	}).done(function(data) { 
+		info = data
+	});
+	return info;	
+}
+var countryInfo = function(countryId) {
+	return fetch('http://api.openaid.se/api/v1/country?id=' + countryId)[0];
+}
+
+var countryContributions = function(countryId) {
+	var ret = {};
+	$(fetch('http://api.openaid.se/api/v1/contribution?country=' + countryId)).each(function(i,v) {
+		ret[v.id] = v
+	});
+	return ret;
+}
+
+var countrySelect = function(sel) {
+	var country = $(sel).val();
+	var info = countryInfo(country);
+	$("h1").text(info['name_swe']);
+	var contributions = countryContributions(country);
+	var perYear = totalPerYear(info, contributions);
+	render(Object.keys(perYear), [dataset(values(perYear))]);
+
 }
 
 $(document).ready(function() {
-	var url = 'http://api.openaid.se/api/v1/country?id=120';
-
-	function dataOfYear(data, year) {
-		return data[0].extra_data.contributions_by_year[year]
-	}
-
-
-	$.ajax({
-		url: url,
-		dataType: "json"
-	}).done(function(data) { 
-		var c = contributions(data);
-		var labels = Object.keys(c);
-		var values = []
-		$.each(c, function(k,v) {
-			values.push(sum(v));
-		});
-		render(labels, values)
-
-	});
+	var GLOBAL = 98;
+	var IRAQ = 120;
+	var info = countryInfo(IRAQ);
+	$("h1").text(info['name_swe']);
+	var contributions = countryContributions(IRAQ)
+	var perYear = totalPerYear(info, contributions);
+	render(Object.keys(perYear), [dataset(values(perYear))]);
 })
